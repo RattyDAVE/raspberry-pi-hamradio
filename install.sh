332c7fc2a7fc8f7727566139b115ec0beea45aa0
RattyDAVE <dave@pucknell.co.uk>
2020-06-21 10:18:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,25 @@
 #Start with Buster with desktop 
 #get from here https://www.raspberrypi.org/downloads/raspbian/
 
+#Also works with arm64 on Raspberry Pi 4. (NOT JS8CALL)
+
+
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 
 #Install Tools - Not needed but good to have.
-sudo apt-get install mc inxi
-
-#Install Ham radio stuff. 
-sudo apt-get install hamradio-antenna hamradio-datamodes hamradio-digitalvoice hamradio-morse hamradio-nonamateur hamradio-packetmodes hamradio-rigcontrol hamradio-satellite hamradio-sdr hamradio-tools linpac wsjtx gqrx-sdr
+sudo apt-get install -y mc inxi
 
 #Install build tools
-sudo apt-get install build-essential cmake libusb-1.0-0-dev git libx11-dev -y
+sudo apt-get install -y build-essential cmake libusb-1.0-0-dev git libx11-dev -y
+
+#Install Ham radio stuff. 
+sudo apt-get install -y hamradio-antenna hamradio-datamodes hamradio-digitalvoice hamradio-morse hamradio-nonamateur hamradio-packetmodes hamradio-rigcontrol hamradio-satellite hamradio-sdr hamradio-tools linpac wsjtx gqrx-sdr
 
 #Create build directory
-cd ~
 mkdir ~/build
-cd build
+cd ~/build
 
 #RTL-SDR Driver
 echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
